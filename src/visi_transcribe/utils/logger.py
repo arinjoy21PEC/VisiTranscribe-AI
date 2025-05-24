298c99be73f97a29f3c5e8208f6b87352b47ddca
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2025-05-25 01:29:13 +0530

    initial commit

--- src/visi_transcribe/utils/logger.py
+++ src/visi_transcribe/utils/logger.py
@@ -7,13 +7,17 @@ os.makedirs(LOG_DIR, exist_ok=True)
 
 log_file = os.path.join(LOG_DIR, f"visi_transcribe_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
 
-logging.basicConfig(
-    level = logging.INFO,
-    format = "%(asctime)s - %(levelname)s - %(message)s",
-    handlers = [
-        logging.FileHandler(log_file),
-        logging.StreamHandler()
-    ]
-)
 
 logger = logging.getLogger("visi_transcribe")
+logger.setLevel(logging.INFO)
+
+
+if not logger.handlers:
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
