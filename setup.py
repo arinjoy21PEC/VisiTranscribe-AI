913eb51f77d4af37f79be808efc2108540034feb
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2025-05-25 17:49:29 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     version = "1.0",
     author = "Arinjoy Nandy",
     author_email = "arinjoynandy2019@gmail.com",
-    packages = find_packages(),
+    packages = find_packages(where="src"),
+    package_dir = {"": "src"},
     install_requires = get_requirements("requirements.txt")
 )
