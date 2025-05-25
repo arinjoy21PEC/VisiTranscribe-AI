853a7d26c20aa11c545add870546695a06eb0488
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2025-05-25 17:33:39 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str):
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 setup(
-    name = 'VisiTranscrible AI',
-    Version = '1.0',
-    packages = find_packages(where='src'),
-    package_dir = {"": "src"},
-    install_requires = [
-        "fastapi",
-        "uvicorn",
-        "transformers",
-        "torch",
-        "pyyaml",
-        "pillow",
-        "python-multipart",
-        "pydub",
-        "openai-whisper",
-        "tqdm",
-        "sentencepiece"
-    ],
+    name = "VisiTranscrible_AI",
+    version = "1.0",
+    author = "Arinjoy Nandy",
+    author_email = "arinjoynandy2019@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
 )
